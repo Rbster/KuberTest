3ac072be4f9e811fb612bac9c926d781104f32ac
Roman <rbster@yandex.ru>
2023-07-01 14:32:42 +0300

    added rest echo endpoints

--- second/src/main/kotlin/ru/test/second/SecondRestController.kt
+++ second/src/main/kotlin/ru/test/second/SecondRestController.kt
@@ -1,16 +1,16 @@
-package ru.test.first
+package ru.test.second
 
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("api/v1")
-class FirstRestController {
-    val log = LoggerFactory.getLogger(FirstRestController::class.java)
+class SecondRestController {
+    val log = LoggerFactory.getLogger(SecondRestController::class.java)
 
     @GetMapping("echo")
     fun echo(s: String): String {
-        log.info("Got message < $s > on the First service")
+        log.info("Got message < $s > on the Second service")
         return s
     }
 }
