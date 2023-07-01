5c37a76f3451e819de0f2f795deb92833fe674f6
Roman <rbster@yandex.ru>
2023-07-01 14:37:23 +0300

    echo services updated

--- first/src/main/kotlin/ru/test/first/FirstRestController.kt
+++ first/src/main/kotlin/ru/test/first/FirstRestController.kt
@@ -2,14 +2,15 @@ package ru.test.first
 
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestAttribute
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("api/v1")
 class FirstRestController {
     val log = LoggerFactory.getLogger(FirstRestController::class.java)
 
-    @GetMapping("echo")
-    fun echo(s: String): String {
+    @GetMapping("echo/\${s}")
+    fun echo(@RequestAttribute s: String): String {
         log.info("Got message < $s > on the First service")
         return s
     }
--- second/src/main/kotlin/ru/test/second/SecondRestController.kt
+++ second/src/main/kotlin/ru/test/second/SecondRestController.kt
@@ -2,15 +2,22 @@ package ru.test.second
 
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestAttribute
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("api/v1")
 class SecondRestController {
     val log = LoggerFactory.getLogger(SecondRestController::class.java)
 
-    @GetMapping("echo")
-    fun echo(s: String): String {
+    @GetMapping("echo/\${s}")
+    fun echo(@RequestAttribute s: String): String {
         log.info("Got message < $s > on the Second service")
         return s
     }
+
+//    @GetMapping("echo")
+//    fun echo(s: String): String {
+//        log.info("Got message < $s > on the Second service")
+//        return s
+//    }
 }
